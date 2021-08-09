18bb16e9caa2e45ab60b6adac09e951b53298f6c
cmallen2106 <cmallen2106@gmail.com>
2021-08-09 20:13:17 +0100

    Create input parameter

--- lab6Ex1/src/lab6Ex1/Lab6Ex1.java
+++ lab6Ex1/src/lab6Ex1/Lab6Ex1.java
@@ -1,9 +1,24 @@
+//Christina Sosa
+//Programming Fundamentals - Summer 2021
+//Lab 6
+
+// Write the simplest Java method that would have 𝑂(𝑁^3) time complexity, where 𝑁 is the single
+//input parameter to the method.
+
+
+
 package lab6Ex1;
+import java.util.Scanner; 
+
+import java.util.Scanner;
 
 public class Lab6Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in); 
+		
+		System.out.print("Enter a value for n: ");
+		int n = scan.nextInt(); 
 
 	}
 
---
+++ lab6Ex5/src/lab6Ex5/Lab6Ex5.java
@@ -0,0 +1,10 @@
+package lab6Ex5;
+
+public class Lab6Ex5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
